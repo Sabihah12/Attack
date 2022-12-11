4938af907e875b88e3be251fb68403d815d7b9d5
sabihahamir12 <sabihahamir12@gmail.com>
2022-12-11 22:50:34 +0800

    initial commit

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-# hello..testing
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html')
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
