e477abc1296a49352df25fbe41701e12d735e765
sabihahamir12 <sabihahamir12@gmail.com>
2022-12-12 17:43:43 +0800

    set up db on fake login

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
 from flask import Flask, render_template, url_for, redirect
+from flask_sqlalchemy import SQLAlchemy
 from form import LoginForm, LoginForm1
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '57bf24e23df89d5eab59adc875b50b76'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(30), nullable=False, unique=True)
+    password = db.Column(db.String(60), nullable=False)
+    
+
+    def __repr__(self):
+        return f"User( '{self.username}' )"
 
 @app.route("/")
 @app.route("/home")
@@ -13,8 +25,10 @@ def home():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        if form.username.data == 'JohnDoe' and form.password.data == 'Ih812changing!':
-            return redirect (url_for('error_404'))
+        user = User(username=form.username.data, password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        return redirect (url_for('error_404'))
     return render_template('fake_login/login.html', title='Login', form=form)
 
 @app.route("/error_404")
